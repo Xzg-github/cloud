d24d83e438cba180842feaae1bc04ca55e100559
zhangyanwei <zhangyanwei@hercules-logistics.com>
2018-07-26 18:19:26 +0800

    添加ePLD页面相关图片以及Node服务器支持路由ePLD页面

--- src/server.js
+++ src/server.js
@@ -10,9 +10,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const routes = [
+  '/production/epld',
+];
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  if (routes.includes(req.path)) {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  } else {
+    next(createError(404));
+  }
 });
 
 // error handler
