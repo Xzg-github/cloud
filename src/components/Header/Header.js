296f8b441843cdc52c5a793c79062604c98eabcf
zhangyanwei <zhangyanwei@hercules-logistics.com>
2018-08-08 12:36:10 +0800

    导航适应手机屏幕

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,23 +4,54 @@ import {Link} from 'react-router';
 import {Icon, Dropdown, Menu} from 'antd';
 import './Header.less';
 
+const imageUrl = (title) => {
+  return `/imgs/${title}.png`;
+};
+
 class Header extends React.Component {
   static propTypes = {
     logoUrl: PropTypes.string.isRequired,
     items: PropTypes.array.isRequired
   };
 
-  state = {visible: ''};
+  state = {visible: '', mobile: false};
 
   onMenuClick = () => {
     this.setState({visible: ''});
   };
 
+  onMobileMenuClick = () => {
+    this.setState({mobile: !this.state.mobile});
+  };
+
+  menuItem = (item) => {
+    if (item.children) {
+      return <Menu.SubMenu key={item.key} title={item.title}>{item.children.map(this.menuItem)}</Menu.SubMenu>;
+    } else {
+      return <Menu.Item key={item.key}><Link to={item.to}>{item.title}</Link></Menu.Item>;
+    }
+  };
+
   menu = (key, children) => {
     return (
       <Menu className='Header-menu' onClick={this.onMenuClick}>
-        {children.map((item) => <Menu.Item key={item.key}><Link to={item.to}>{item.title}</Link></Menu.Item>)}
+        {children.map(this.menuItem)}
       </Menu>
+    );
+  };
+
+  mobileMenu = (items) => {
+    const onClick = (e) => {
+      if (e.target.getAttribute('data-role') === 'mobile-menu') {
+        this.setState({mobile: false});
+      }
+    };
+    return (
+      <div data-role='mobile-menu' onClick={onClick}>
+        <Menu mode='inline' onClick={() => this.setState({mobile: false})}>
+          {items.map(this.menuItem)}
+        </Menu>
+      </div>
     );
   };
 
@@ -51,6 +82,8 @@ class Header extends React.Component {
         <div>
           <Link to='/'><img src={logoUrl} alt='logo'/></Link>
           <span>{items.map(this.navItem)}</span>
+          <img src={imageUrl(this.state.mobile ? 'close' : 'menu')} alt='menu' onClick={this.onMobileMenuClick} />
+          {this.state.mobile ? this.mobileMenu(items) : null}
         </div>
       </header>
     );
